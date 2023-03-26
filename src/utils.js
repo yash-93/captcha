c8feb8e7e1a9d57fad0509cb8288c5175a445d76
Yashdeep Bachhas <yashdeepbachhas93@gmail.com>
2023-03-26 22:32:08 +0530

    added fields validation

---
+++ src/utils.js
@@ -0,0 +1,42 @@
+export const checkFieldValidity = (values, generatedCaptcha) => {
+  const keys = Object.keys(values);
+  const vals = Object.values(values);
+  let err = [];
+  keys.forEach((key, index) => {
+    switch (key) {
+      case 'username':
+        if (!vals[index]) {
+          err.push('Username cannot be empty.');
+        }
+        break;
+      case 'email':
+        if (!vals[index]) {
+          err.push('Email cannot be empty.');
+        } else if (!vals[index].match(/\S+@\S+\.\S+/)) {
+          err.push('Invalid email.');
+        }
+        break;
+      case 'password':
+        if (!vals[index]) {
+          err.push('Password cannot be empty.');
+        } else if (vals[index].length < 8) {
+          err.push('Password can not be less than 8 characters.');
+        }
+        break;
+      case 'captcha':
+        if (!vals[index]) {
+          err.push('Captcha cannot be empty.');
+        } else if (vals[index] !== generatedCaptcha) {
+          err.push('Captcha do not match');
+        }
+        break;
+      default:
+        return err;
+    }
+  });
+  return err;
+}
+
+export const generateCaptcha = () => {
+  return '12345';
+}
